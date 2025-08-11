c06acd6d879f0f907132ab916aedeeab2ef8fd85
Tyler Gardiner <78093138+tyler-g-AVGroup@users.noreply.github.com>
2025-08-11 10:15:29 -0400

    Update push_nuget.sh

--- plugincompile/push_nuget.sh
+++ plugincompile/push_nuget.sh
@@ -2,6 +2,7 @@ infoLua=$(printf "$1\\%s" "01-info.lua")
 # echo "infoLua is $infoLua"
 
 build_version=$(grep 'BuildVersion' $infoLua | sed -E 's/.*"([^"]+)".*/\1/')
+build_version=${build_version%\.0} #strip the trailing ".0" to comply with nuget semver normalization( https://learn.microsoft.com/en-us/nuget/concepts/package-versioning?tabs=semver20sort#normalized-version-numbers )
 
 nupkg=$(printf "%s.%s.%s" $2 $build_version "nupkg") 
 
